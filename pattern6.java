aee88959c6ac4f973f780936599ddc36a4e11720
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-08-25 21:19:55 +0530

    Create pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,16 @@
+
+public class pattern6 {
+    public static void main(String[] args) {
+        int k=6,y=1;
+        for(k=6;k>=1;k--)
+        {
+            for(y=1;y<=k;y++)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
